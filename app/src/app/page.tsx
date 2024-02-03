95340d6e9312ff15528e2e7b62290c9081331599
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-02-03 18:03:47 -0500

    Change style for dekstop widths

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="p-20">
-      <div className="flex flex-col  w-7/12 gap-24">
+      <div className="flex flex-col gap-16">
         <h1 className="text-9xl">Need One More?</h1>
         <div>
           <h2 className="text-6xl underline">Find Listings</h2>
           <h2 className="text-6xl underline">Connect with Students</h2>
         </div>
-        <p className="text-4xl">
+        <p className="text-4xl max-w-[35ch]">
           Unlock Your Ideal Home: Welcome to Housify, Where Queen&apos;s
           Students Come Together to List and Find the Perfect Living Spaces!
         </p>
@@ -18,12 +18,12 @@ export default function Home() {
           Get Started
         </button>
       </div>
-      <div className="absolute right-0 top-10">
+      <div className="absolute right-0 top-20">
         <Image
           src="person-sitting.svg"
           alt="Icon of person sitting at home"
-          width={800}
-          height={800}
+          width={650}
+          height={650}
         />
       </div>
     </div>
