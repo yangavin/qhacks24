46bbd6de26011f74de6913d019832fe56b591308
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-02-04 00:01:51 -0500

    Protect information routes with auth0

--- app/src/app/seeker-info/page.tsx
+++ app/src/app/seeker-info/page.tsx
@@ -1,19 +1,23 @@
 import "./styles.css";
 import Image from "next/image";
 import { Form } from "./Form";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-export default function PosterInfo() {
-  return (
-    <>
-      <Form />
-      <div className="absolute right-0 top-10">
-        <Image
-          src="person-sitting.svg"
-          alt="Icon of person sitting at home"
-          width={800}
-          height={800}
-        />
-      </div>
-    </>
-  );
-}
+export default withPageAuthRequired(
+  async function PosterInfo() {
+    return (
+      <>
+        <Form />
+        <div className="absolute right-0 top-10">
+          <Image
+            src="person-sitting.svg"
+            alt="Icon of person sitting at home"
+            width={800}
+            height={800}
+          />
+        </div>
+      </>
+    );
+  },
+  { returnTo: "/seeker-info" }
+);
