26defb1346cd4642650626a13b8ac736e79ace5b
Sabin Pokhrel <118865601+sabinpok@users.noreply.github.com>
2024-02-03 23:12:40 -0500

    Add info pages for house seekers and posters

---
+++ app/src/app/seeker-info/page.tsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+import Image from "next/image";
+import { Form } from "./Form";
+
+export default function PosterInfo() {
+  return (
+    <>
+      <Form />
+      <div className="absolute right-0 top-10">
+        <Image
+          src="person-sitting.svg"
+          alt="Icon of person sitting at home"
+          width={800}
+          height={800}
+        />
+      </div>
+    </>
+  );
+}
