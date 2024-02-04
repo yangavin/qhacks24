e0958a2c847a30c73614d57292218c521fafeb4d
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-02-04 06:25:40 -0500

    Change allowed hosts

--- python-scrape/myproject/settings.py
+++ python-scrape/myproject/settings.py
@@ -130,3 +130,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+ALLOWED_HOSTS = ["https://qhacks24-git-main-yangavin.vercel.app/", "159.203.17.167"]
