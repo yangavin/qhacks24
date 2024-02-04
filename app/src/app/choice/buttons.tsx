aa28e0cf93528a3800137d1637c9f15db7780467
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-02-04 00:21:21 -0500

    Route choice buttons to corresponding info page

--- app/src/app/choice/buttons.tsx
+++ app/src/app/choice/buttons.tsx
@@ -1,18 +1,20 @@
+import Image from "next/image";
+
 export function Buttons() {
   return (
     <div className="button-container">
-      <button className="btn" role="button">
+      <a href="/seeker-info" className="btn" role="button">
         <div className="button-image">
           <img src="seeker.svg" alt="" />
         </div>
         <div className="button-text">
           <span className="btn-text"> Find a Place</span>
         </div>
-      </button>
-      <button className="btn" role="button">
+      </a>
+      <a href="/poster-info" className="btn" role="button">
         <img src="house.svg" alt="" />
-        <span className="btn-text"> Find a Roommate</span>
-      </button>
+        <span className="btn-text">Find a Roommate</span>
+      </a>
     </div>
   );
 }
